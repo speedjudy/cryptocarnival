042703ddb611d85af14e2eb5f17545f463ec3238
speed <speedjudy928@gmail.com>
2023-03-23 02:13:58 +0900

    first commit

---
+++ src/views/index.js
@@ -0,0 +1,8 @@
+export { default as Temp } from "./Temp/index";
+export { default as Contest } from "./Contest/index";
+export { default as Comingsoon } from "./Comingsoon/index";
+export { default as Thankyou } from "./Thankyou/index";
+export { default as Dice } from "./Dice/index";
+export { default as About } from "./About/index";
+export { default as Faucet } from "./Faucet/index";
+export { default as nabben } from "./nabben/index";
